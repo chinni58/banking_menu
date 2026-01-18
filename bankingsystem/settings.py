d044be1dda5bb8b34ec76c54453fdd620e854e04
thriveni <sthriveni58@gmail.com>
2026-01-19 00:40:46 +0530

    Fix ALLOWED_HOSTS for Render

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z+4zk(6dl4=^y2qq^88!fjhp!e9z1#77r2a#&n_gf@crl6zi+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
